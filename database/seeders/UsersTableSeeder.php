a9fe5f295270c08150f37e06c76a467cc84685ea
Prajwal100 <iar.prajwal@gmail.com>
2021-01-11 21:33:30 +0545

    initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+
+//            Admin
+            [
+                'full_name'=>'Prajwal Admin',
+                'username'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'admin',
+                'status'=>'active',
+            ],
+//            Vendor
+            [
+                'full_name'=>'Prajwal Vendor',
+                'username'=>'Vendor',
+                'email'=>'vendor@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'vendor',
+                'status'=>'active',
+            ],
+//            Customer
+            [
+                'full_name'=>'Prajwal Customer',
+                'username'=>'Customer',
+                'email'=>'customer@gmail.com',
+                'password'=>Hash::make('111'),
+                'role'=>'customer',
+                'status'=>'active',
+            ],
+        ]);
+    }
+}
